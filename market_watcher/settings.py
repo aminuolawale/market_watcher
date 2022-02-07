2b9a3fd7cf5497541a135f092caaaed77162ac8b
wizzle3d <wizzle3d@gmail.com>
2022-02-07 15:21:22 +0100

    token lifetime now 60mins

--- market_watcher/settings.py
+++ market_watcher/settings.py
@@ -168,7 +168,7 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
